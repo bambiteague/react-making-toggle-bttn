823647100710ccabeb1709805d2759040fba3422
Bambi James Teague <72958518+bambiteague@users.noreply.github.com>
2021-10-11 16:17:58 -0500

    styling of button to create toggle effect

--- src/App.js
+++ src/App.js
@@ -5,8 +5,25 @@ export default function App() {
 
   return (
     <div>
-      <button onClick={(e) => setToggleBttn(!toggleBttn)}>
-        this is button
+      <button
+        onClick={(e) => setToggleBttn(!toggleBttn)}
+        style={{
+          height: "2rem",
+          width: "4rem",
+          borderRadius: "2rem",
+          background: toggleBttn ? "peachpuff" : "mediumturquoise"
+        }}
+      >
+        <div
+          style={{
+            height: "1.4rem",
+            width: "1.4rem",
+            border: "solid 1px gray",
+            borderRadius: "2rem",
+            background: toggleBttn ? "mediumturquoise" : "peachpuff",
+            transform: toggleBttn ? "translateX(1.4rem)" : ""
+          }}
+        />
       </button>
       <h1>{toggleBttn ? "ON" : "OFF"}</h1>
     </div>
